7a58f19a15fde2094f86422b39810c560ec1ec44
Ceren Yılmaz <ceren.yilmaz@studenti.unipd.it>
2024-08-17 22:23:00 +0200

    navbar-update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Navbar.css';
 import logo from '../assets/bookfinder_logo-removebg.png';
 import { CgProfile } from "react-icons/cg";
@@ -6,8 +6,26 @@ import { MdFavoriteBorder } from "react-icons/md";
 import { TiThList } from "react-icons/ti";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiGhost2Fill } from 'react-icons/ri';
+import { Switch } from '@mui/material';
+
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(false);
+  const [search, setSearch] = useState(false);
+  const handleTheme = () =>{
+    setTheme(!theme);
+  };
+
+  const handleSearch = () =>{
+    setSearch(!search);
+  }; 
+  useEffect(() =>{
+    if (theme) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [theme]);
   return (
     <div className='navbar-container'>
       <div className='left-corner'>
@@ -16,10 +34,15 @@ const Navbar = () => {
       </div>
 
       <div className='right-corner'>
-        <BiSearchAlt size={30} />
+        <div className={`search-box ${search ? '' : 'visible'}`}>
+          <input name='entity' className='search-input' type='text' placeholder='Search...'/>
+          <BiSearchAlt size={35} className='search-icon' onClick={handleSearch}/>
+        </div>
+        <BiSearchAlt size={30} onClick={handleSearch}/>
         <TiThList size={30} />
-        <MdFavoriteBorder size={30}/>
-        <CgProfile size={30} className='profile-icon'/>
+        <MdFavoriteBorder size={30} />
+        <Switch onChange={handleTheme} className='toggle-button' size='medium' color='#E7B7A0'/>
+        <CgProfile size={30} className='profile-icon' />
       </div>
     </div>
   )
